ad4dede366bea2f0fb3b87ae839fcf300821e24b
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2026-02-20 17:17:09 -0500

    code editor

--- levels/slides.gd
+++ levels/slides.gd
@@ -4,9 +4,16 @@ var current_slide=0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_slide(0)
 
+func set_slide(new_slide):
+	for slide in get_children():
+		slide.visible=false
+	current_slide=new_slide
+	get_child(current_slide).visible=true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_next_pressed() -> void:
+	if current_slide<get_child_count()-1:
+		set_slide(current_slide+1)
+	else:
+		hide()
